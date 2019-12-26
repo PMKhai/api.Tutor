0546e91a6557338d376c77ffc1a0dc8e57235b19
unknown <huycong2798@gmail.com>
2019-12-26 10:55:48 +0700

    change url to host to deploy

--- config/url.js
+++ config/url.js
@@ -1,5 +1,5 @@
-const backend = 'http://localhost:8000/';
-const frontend = 'http://localhost:3000/';
-// const backend =  'https://server-api-tutor.herokuapp.com/';
-// const frontend = 'https://tutor-reactjs.firebaseapp.com/home';
+// const backend = 'http://localhost:8000/';
+// const frontend = 'http://localhost:3000/';
+const backend =  'https://server-api-tutor.herokuapp.com/';
+const frontend = 'https://tutor-reactjs.firebaseapp.com/home';
 module.exports = {backend,frontend}
